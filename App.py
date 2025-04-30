0210844405b17a81b42ac848ad4b097713fb5c36
Enapoles02 <enrique.napoles@dbschenker.com>
2025-04-30 13:38:24 -0600

    Update App.py

--- App.py
+++ App.py
@@ -223,7 +223,7 @@ if not df.empty:
 footer_text = f"""
     <div style="position:fixed; bottom:0; left:0; width:100%; text-align:center;
     padding:6px; font-size:0.75rem; color:#888; background-color:#f8f8f8;">
-    Last update: {last_update if isinstance(last_update, str) else last_update.strftime('%Y-%m-%d %H:%M:%S') if last_update else "–"}
+    Last update: {last_update if isinstance(last_update, str) else (last_update.strftime('%Y-%m-%d %H:%M:%S') if last_update else "–")}
     </div>
 """
 st.markdown(footer_text, unsafe_allow_html=True)
