fd1abcc7cf2cb9cedd7ebb00f990d355cad9c7d0
Enapoles02 <enrique.napoles@dbschenker.com>
2025-05-06 09:07:45 -0600

    Update App.py

--- App.py
+++ App.py
@@ -126,7 +126,6 @@ def upload_to_firestore(df):
         "data": df_clean.to_dict(orient="records"),
        now_cdmx = datetime.now(MEXICO_TZ)
 "last_update": now_cdmx.strftime("%Y-%m-%d %H:%M:%S %Z")
-
     })
 
 def download_from_firestore():
